c9f4dd0baac94d10f33c2829b706e50a01c04813
junhnno <withardor03@gmail.com>
2026-04-03 20:06:22 +0900

    Django 세션 실습

--- main/views.py
+++ main/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+def mainpage(request):
+    return render(request, 'main/mainpage.html')
+
+def mainpage(request):
+    context = {
+        'generation': 14,                   
+        'reviews': [
+            'Templates 폴더 생성',
+            'views.py 작성 -> request로 불러올 페이지 설정',
+            'urls.py 작성 -> client가 보는 화면 설정',
+            'Template 언어 -> 반복문, 상속',
+            'navbar 상속',
+            'static 폴더 생성 -> css, image',
+        ]
+    }
+    return render(request, 'main/mainpage.html', context)
+
+def secondpage(request):
+    return render(request, 'main/secondpage.html')
